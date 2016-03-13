7fbe8941fcc1e7e8e7b149e1e5af1e23d3db0c26
Todd Utterback <todd.j.utterback@gmail.com>
2016-03-12 17:26:46 -0700

    for the win

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -15,7 +15,7 @@ class HabitsController < ApplicationController
   def create
     @habit = current_user.habits.new(habit_params)
     if @habit.save
-      redirect_to habit_path
+      redirect_to habit_path(@habit)
     else
       render :new
     end
